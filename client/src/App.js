51a69cd6808fa30baab0ec470f40e1a1fd1e0eba
Kyprosa <propolis200@gmail.com>
2023-08-01 23:27:33 +0900

    8강

--- client/src/App.js
+++ client/src/App.js
@@ -20,32 +20,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id' :1,
-  'iamge' : "https://placeimg.com/64/64/any",
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id' :2,
-  'iamge' : "https://placeimg.com/64/64/2",
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-},
-{
-  'id' :3,
-  'iamge' : "https://placeimg.com/64/64/3",
-  'name':'홍길동',
-  'birthday':'961222',
-  'gender':'남자',
-  'job':'대학생'
-}
-]
 class App extends Component {
+
+state = {
+  customers: ""
+}
+
+componentDidMount(){
+  this.callApi()
+  .then(res => this.setState({customers: res}))
+  .catch(err => console.log(err));
+}
+
+callApi = async() => {
+  const response = await fetch('/api/customers');
+  const body = await response.json();
+  return body;
+}
+
   render() { 
     const {classes} = this.props;
     return (
@@ -62,7 +54,7 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-          {customers.map(c => {
+          {this.state.customers ? this.state.customers.map(c => {
             return(
               <Customer
               key={c.id}
@@ -74,7 +66,7 @@ class App extends Component {
               job={c.job}
               />
             )
-          })}
+          }) : ""}
           </TableBody>
         </Table>
       </Paper>
